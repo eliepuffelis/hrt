f465c2b03d4ec39c9047fc1c3a9a6fc39a5c4267
Elie Puffelis <eliepuffelis@gmail.com>
2019-08-31 17:19:32 -0700

    disabled scrolling when menu opens

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 
-var menu = document.getElementById("body");
+var body = document.getElementById("body");
 var menu = document.getElementById("menu");
 var menuMobile = document.getElementById("menu-mobile");
 var darkOverlay = document.getElementById("menu-mobile-dark-overlay");
 var menuOpen = false;
 
+
+
+/*
+* menu functionality ==========================================================
+*/
 function menubars() {
     
     if(!menuOpen)
@@ -17,6 +22,7 @@ function openMenu(){
     menuMobile.style.visibility = 'visible';
     menuMobile.style.right = '0';
     darkOverlay.style.visibility = 'visible'
+    body.style.overflow = 'hidden'
     menuOpen = true; 
 
 }
@@ -25,8 +31,36 @@ function closeMenu() {
     menuMobile.style.visibility = 'hidden';
     menuMobile.style.right = '-100vw';
     darkOverlay.style.visibility = 'hidden'
+    body.style.overflow = 'scroll';
     menuOpen = false;
 }
+
+/*
+* link smooth scrolling ==========================================================
+*/
+
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// $('a[href*="#"]').on('click', function (e) {
+//     e.preventDefault()
+
+//     $('html, body').animate(
+//         {
+//             scrollTop: $($(this).attr('href')).offset().top,
+//         },
+//         500,
+//         'linear'
+//     )
+// })
 
 
 
