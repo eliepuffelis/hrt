f66edb16f558e68ba75a3f073115ee4bd1eaa985
Elie Puffelis <eliepuffelis@gmail.com>
2019-09-02 16:26:54 -0700

    created dist folder with all root files

---
+++ dist/main.js
@@ -0,0 +1,52 @@
+
+var body = document.getElementById("body");
+var menu = document.getElementById("menu");
+var menuMobile = document.getElementById("menu-mobile");
+var darkOverlay = document.getElementById("menu-mobile-dark-overlay");
+var menuOpen = false;
+
+
+
+/*
+* menu functionality ==========================================================
+*/
+function menubars() {
+    
+    if(!menuOpen)
+        openMenu();
+    else
+        closeMenu();
+}
+
+function openMenu(){
+    menu.style.visibility = 'visible';
+    menu.style.right = '0';
+    menuMobile.style.visibility = 'visible';
+    menuMobile.style.right = '0';
+    darkOverlay.style.visibility = 'visible'
+    body.style.overflow = 'hidden'
+    menuOpen = true; 
+}
+
+function closeMenu() {
+    menu.style.visibility = 'hidden';
+    menu.style.right = '-100vw';
+    menuMobile.style.visibility = 'hidden';
+    menuMobile.style.right = '-100vw';
+    darkOverlay.style.visibility = 'hidden'
+    body.style.overflow = 'scroll';
+    menuOpen = false;
+}
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 2000,
+    speedAsDuration: true,
+    header: '[navbar]'
+});
+
+
+
+
+
+
